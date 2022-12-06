781086da20e5236f6aca2db4e3fc50fce6337a75
yuliya <gyuliya2001@gmail.com>
2022-12-07 00:56:48 +0300

    show online-requests list in user cabinet

--- src/main/java/com/golubovich/project_trpo_tofi/controller/PersonalCabinetController.java
+++ src/main/java/com/golubovich/project_trpo_tofi/controller/PersonalCabinetController.java
@@ -1,18 +1,27 @@
 package com.golubovich.project_trpo_tofi.controller;
 
+import com.golubovich.project_trpo_tofi.model.Request;
 import com.golubovich.project_trpo_tofi.model.User;
+import com.golubovich.project_trpo_tofi.repository.RequestRepository;
 import com.golubovich.project_trpo_tofi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Set;
 
 @Controller
 public class PersonalCabinetController  {
     @Autowired
     private UserRepository userRepository;
+//    @Autowired
+//    private RequestRepository requestRepository;
 
     @GetMapping("/cabinet")
     public String cabinet(Model model) {
@@ -21,6 +30,8 @@ public class PersonalCabinetController  {
         model.addAttribute("user", user);
         model.addAttribute("modalWindowContent", "user-request-response");
         model.addAttribute("modalWindowPath", "blocks/user-requests-list");
+        model.addAttribute("modalWindowBtnText", "Показать");
         return "personal-cabinet";
     }
+
 }
